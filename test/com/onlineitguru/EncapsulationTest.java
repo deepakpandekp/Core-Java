d805d150ae5d4fecee7885495b1a728dc4c3378b
Deepak Pande <deepakpandekp@gmail.com>
2020-04-18 19:54:31 +0530

    adding unit test

---
+++ test/com/onlineitguru/EncapsulationTest.java
@@ -0,0 +1,16 @@
+package com.onlineitguru;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.onlineitguru.encapsulation.Employee;
+
+public class EncapsulationTest {
+	@Test
+	public void EmployeeTest() {
+		Employee employee = new Employee("John", 27,"Newyork");
+		employee.printEmployeDetails();
+		assertEquals("John", employee.name);
+	}
+
+}
